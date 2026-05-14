80db3952e0497c2f47b06400419c6ceefe8bebac
SengokuCola <1026294844@qq.com>
2026-05-14 18:15:48 +0800

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -174,7 +174,7 @@ class PluginSectionConfig(PluginConfigBase):
     __ui_order__ = 0
 
     enabled: bool = Field(default=True, description="是否启用插件")
-    config_version: str = Field(default="4.5.0", description="配置版本")
+    config_version: str = Field(default="4.5.1", description="配置版本")
 
 
 class ComponentsConfig(PluginConfigBase):
